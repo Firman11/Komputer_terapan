18c1afa3fb7018cc389e434c0c03b929b3e63332
Firman11 <Andres.firman115@gmail.com>
2015-05-27 02:20:46 -0700

    Update Nur_firmansyah_xitkj4_blinkingledarduino.ino

--- Nur_firmansyah_xitkj4_blinkingledarduino.ino
+++ Nur_firmansyah_xitkj4_blinkingledarduino.ino
@@ -1,29 +1,29 @@
-int firman3 = 3;
-int firman7 = 7;
-int firman8 = 8;
-int firman9 = 9;
+int varled3 = 3;
+int varled7 = 7;
+int varled8 = 8;
+int varled9 = 9;
 void setup() {
-pinMode(firman3, OUTPUT);
-pinMode(firman7, OUTPUT);
-pinMode(firman8, OUTPUT);
-pinMode(firman9, OUTPUT);
+pinMode(varled3, OUTPUT);
+pinMode(varled7, OUTPUT);
+pinMode(varled8, OUTPUT);
+pinMode(varled9, OUTPUT);
 }
 void loop() {
-{digitalWrite(firman3, HIGH);
+{digitalWrite(varled3, HIGH);
 delay(100);
-digitalWrite(firman3, LOW);
+digitalWrite(varled3, LOW);
 delay(100);}
-{digitalWrite(firman7, HIGH);
+{digitalWrite(varled7, HIGH);
 delay(100);
-digitalWrite(firman7, LOW);
+digitalWrite(varled7, LOW);
 delay(100);}
-{digitalWrite(firman8, HIGH);
+{digitalWrite(varled8, HIGH);
 delay(100);
-digitalWrite(firman8, LOW);
+digitalWrite(varled8, LOW);
 delay(100);}
-{digitalWrite(firman9,HIGH);
+{digitalWrite(varled9,HIGH);
 delay(100);
-digitalWrite(firman9,LOW);
+digitalWrite(varled9,LOW);
 delay(100);
 }
 }

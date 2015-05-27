6c657c8b4f3325353bc1220a4f6e78988db55871
Firman11 <Andres.firman115@gmail.com>
2015-05-27 02:26:41 -0700

    Update Nur_firmansyah_xitkj4_blinkingledarduino.ino

--- Nur_firmansyah_xitkj4_blinkingledarduino.ino
+++ Nur_firmansyah_xitkj4_blinkingledarduino.ino
@@ -2,24 +2,28 @@ int varled3 = 3;
 int varled7 = 7;
 int varled8 = 8;
 int varled9 = 9;
+
 void setup() {
-pinMode(varled3, OUTPUT);
-pinMode(varled7, OUTPUT);
-pinMode(varled8, OUTPUT);
-pinMode(varled9, OUTPUT);
+  
+  pinMode(varled3, OUTPUT);
+  pinMode(varled7, OUTPUT);
+  pinMode(varled8, OUTPUT);
+  pinMode(varled9, OUTPUT);
 }
+
 void loop() {
-{digitalWrite(varled3, HIGH);
-delay(100);
-digitalWrite(varled3, LOW);
-delay(100);}
-{digitalWrite(varled7, HIGH);
-delay(100);
-digitalWrite(varled7, LOW);
-delay(100);}
-{digitalWrite(varled8, HIGH);
-delay(100);
-digitalWrite(varled8, LOW);
+  
+  {digitalWrite(varled3, HIGH);
+  delay(100);
+  digitalWrite(varled3, LOW);
+  delay(100);}
+  {digitalWrite(varled7, HIGH);
+  delay(100);
+  digitalWrite(varled7, LOW);
+  delay(100);}
+  {digitalWrite(varled8, HIGH);
+  delay(100);
+  digitalWrite(varled8, LOW);
 delay(100);}
 {digitalWrite(varled9,HIGH);
 delay(100);
